41f29484e000583f32973b20c6df5dbe36fcae13
macotok <monochromer@r8.ucom.ne.jp>
2019-04-13 18:06:13 +0900

    add visual slide

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log("test");
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
+window.onload = () => {
+  const pageLInk = () => {
+    $('a[href^="#"]').on('click', function() {
+      const speed = 1000;
+      const href = $(this).attr('href');
+      const target = $(href === '#' || href === '' ? 'html' : href);
+      const position = target.offset().top;
+      $('body, html').animate({
+        scrollTop: position,
+      }, speed, 'swing');
+      return false;
+    });
+  };
+  $('.fn-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    speed: 2000
+  });
+  pageLInk();
+};
