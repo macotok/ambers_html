7697765d4fd209710aab6702667442d4f8e1080e
macotok <monochromer@r8.ucom.ne.jp>
2019-04-22 22:10:27 +0900

    add animation to block

--- src/js/ElementAnimation.js
+++ src/js/ElementAnimation.js
@@ -8,12 +8,14 @@ export default class ElementAnimation {
     this.scrollEventFlag = true;
     this.observeTime = 200;
     this.controlPosition = 500;
+    this.delaySpeed = 150;
   }
   getPositionToBlock() {
     for (let i = 0; i < this.$targetBlock.length; i += 1) {
       this.targetBlockPosition.push(this.$targetBlock.eq(i).offset().top);
     }
     this.eventScroll();
+    this.resizeWindow();
   }
   eventScroll() {
     window.addEventListener('scroll', () => {
@@ -38,25 +40,19 @@ export default class ElementAnimation {
   }
   eventAnimation(targetBlock) {
     const len = targetBlock.find(this.$targetElement).length;
-    const speed = 100;
     for (let i=0; i < len; i += 1) {
-      targetBlock.find(this.$targetElement).eq(i).delay(i * speed).queue(function(){
+      targetBlock.find(this.$targetElement).eq(i).delay(i * this.delaySpeed).queue(function(){
         $(this).addClass('active').dequeue();
       });
     }
   }
-  static judgeDevice() {
-    const ua = navigator.userAgent;
-    if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
-      return 'sp';
-    } else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
-      return 'tab';
-    } else {
-      return 'other';
-    }
-  }
   resizeWindow() {
     window.addEventListener('resize', () => {
-    });
-  };
+      clearTimeout(this.observeResize);
+      this.observeResize = setTimeout(() => {
+        this.targetBlockPosition = [];
+        this.getPositionToBlock();
+      }, this.observeTime);
+    }, false);
+  }
 }
